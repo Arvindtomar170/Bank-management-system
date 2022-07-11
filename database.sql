7a54e37866a4e9c5247747e9728321876442dafb
arvind <arvindbhut170@gmail.com>
2022-07-11 22:08:38 +0530

    file renamed

--- database.sql
+++ database.sql
@@ -1,82 +1,81 @@
-create database banksystem;
-use banksystem;
+CREATE DATABASE banksystem;
+USE banksystem;
+CREATE TABLE Admin
+(
+  Admin_ID INT AUTO_INCREMENT NOT NULL,
+  Admin_name VARCHAR(20) NOT NULL,
+  Contact_no NUMERIC(10, 0) NOT NULL,
+  Address VARCHAR(50),
+  Total_user INT,
+  Email VARCHAR(50) NOT NULL,
+  PRIMARY KEY (Admin_ID)
+);
 
-CREATE TABLE `admin` (
-  `Admin_ID` int NOT NULL AUTO_INCREMENT,
-  `Admin_name` varchar(20) NOT NULL,
-  `Contact_no` decimal(10,0) NOT NULL,
-  `Address` varchar(50) DEFAULT NULL,
-  `Total_user` int DEFAULT NULL,
-  `Email` varchar(50) NOT NULL,
-  PRIMARY KEY (`Admin_ID`)
-) ;
-CREATE TABLE `admin_login_system` (
-  `Login_ID` varchar(20) NOT NULL,
-  `Login_Password` varchar(20) NOT NULL,
-  `Admin_ID` int NOT NULL,
-  PRIMARY KEY (`Login_ID`),
-  UNIQUE KEY `Login_ID` (`Login_ID`),
-  KEY `Admin_ID` (`Admin_ID`),
-  CONSTRAINT `admin_login_system_ibfk_1` FOREIGN KEY (`Admin_ID`) REFERENCES `admin` (`Admin_ID`)
-) ;
-CREATE TABLE `accounts` (
-  `Account_no` int NOT NULL AUTO_INCREMENT,
-  `Account_status` varchar(20) DEFAULT NULL,
-  `Account_name` varchar(20) NOT NULL,
-  `Balance` int NOT NULL,
-  `Interest_rate` int NOT NULL,
-  `Admin_ID` int DEFAULT NULL,
-  `Phone_no` decimal(10,0) NOT NULL,
-  `Address` varchar(50) NOT NULL,
-  `DOB` varchar(20) NOT NULL,
-  `Aadhaar_no` decimal(12,0) NOT NULL,
-  `Voter_ID` int DEFAULT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Account_type` varchar(20) DEFAULT NULL,
-  `Pan_card` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`Account_no`),
-  UNIQUE KEY `Aadhaar_no` (`Aadhaar_no`),
-  UNIQUE KEY `Email` (`Email`),
-  UNIQUE KEY `Voter_ID` (`Voter_ID`),
-  UNIQUE KEY `Pan_card` (`Pan_card`),
-  KEY `Admin_ID` (`Admin_ID`),
-  CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`Admin_ID`) REFERENCES `admin` (`Admin_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `transaction_history` (
-  `Transaction_status` varchar(20) DEFAULT NULL,
-  `Amount` int NOT NULL,
-  `ToAccount_ID` int DEFAULT NULL,
-  `Transaction_date` datetime NOT NULL,
-  `Transaction_ID` int NOT NULL AUTO_INCREMENT,
-  `Transaction_type` varchar(20) NOT NULL,
-  `Account_no` int NOT NULL,
-  PRIMARY KEY (`Transaction_ID`),
-  KEY `Account_no` (`Account_no`),
-  CONSTRAINT `transaction_history_ibfk_1` FOREIGN KEY (`Account_no`) REFERENCES `accounts` (`Account_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `account_login_system` (
-  `Login_ID` int NOT NULL AUTO_INCREMENT,
-  `Login_password` varchar(20) NOT NULL,
-  `Account_no` int NOT NULL,
-  PRIMARY KEY (`Login_ID`),
-  KEY `Account_no` (`Account_no`),
-  CONSTRAINT `account_login_system_ibfk_1` FOREIGN KEY (`Account_no`) REFERENCES `accounts` (`Account_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE Admin_login_system
+(
+  Login_ID VARCHAR(20) UNIQUE NOT NULL,
+  Login_Password VARCHAR(20) NOT NULL,
+  Admin_ID INT NOT NULL,
+  PRIMARY KEY (Login_ID),
+  FOREIGN KEY (Admin_ID) REFERENCES Admin(Admin_ID)
+);
 
-CREATE TABLE `loan` (
-  `Interest` int NOT NULL DEFAULT '12',
-  `Amount` int DEFAULT NULL,
-  `Loan_ID` int NOT NULL AUTO_INCREMENT,
-  `Account_no` int NOT NULL,
-  `Loan_status` varchar(20) DEFAULT NULL,
-  `period` varchar(20) DEFAULT NULL,
-  `request_date` date DEFAULT NULL,
-  `loan_pass_on` date DEFAULT NULL,
-  `amount_passed` int DEFAULT NULL,
-  PRIMARY KEY (`Loan_ID`),
-  KEY `Account_no` (`Account_no`),
-  CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`Account_no`) REFERENCES `accounts` (`Account_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE Accounts
+(
+  Account_no INT NOT NULL auto_increment,
+  Account_status VARCHAR(20) ,
+  Account_name VARCHAR(20) NOT NULL,
+  Balance INT NOT NULL,
+  Interest_rate INT NOT NULL,
+  Admin_ID INT,
+  Phone_no NUMERIC(10) NOT NULL,
+  Address VARCHAR(50) NOT NULL,
+  DOB VARCHAR(20) NOT NULL,
+  Aadhaar_no NUMERIC(12) NOT NULL UNIQUE,
+  Voter_ID INT UNIQUE, 
+  Email VARCHAR(50) NOT NULL UNIQUE,  
+  Account_type VARCHAR(20),
+  Pan_card VARCHAR(10) UNIQUE,
+  PRIMARY KEY (Account_no),  
+  FOREIGN KEY (Admin_ID) REFERENCES Admin(Admin_ID)
+);
+
+CREATE TABLE Transaction_History
+( 
+  Transaction_status VARCHAR(20),
+  Amount INT NOT NULL,
+  ToAccount_ID INT ,
+  Transaction_date DATETIME NOT NULL,
+  Transaction_ID INT AUTO_INCREMENT NOT NULL,
+  Transaction_type VARCHAR(200),
+  Account_no INT NOT NULL,
+  PRIMARY KEY (Transaction_ID),
+  FOREIGN KEY (Account_no) REFERENCES Accounts(Account_no)
+);
+
+CREATE TABLE Loan
+(
+  Interest INT NOT NULL,
+  Amount INT NOT NULL,
+  Loan_ID INT NOT NULL AUTO_INCREMENT,
+  Account_no INT NOT NULL,
+  Loan_status VARCHAR(20),
+  period VARCHAR(20),
+  request_date DATE,
+  loan_pass_on DATE,
+  amount_passed INT,
+  PRIMARY KEY (Loan_ID),
+  FOREIGN KEY (Account_no) REFERENCES Accounts(Account_no)
+);
+
+CREATE TABLE Account_login_system
+(
+  Login_ID INT NOT NULL AUTO_INCREMENT,
+  Login_password VARCHAR(20) NOT NULL,
+  Account_no INT NOT NULL,
+  PRIMARY KEY (Login_ID),
+  FOREIGN KEY (Account_no) REFERENCES Accounts(Account_no)
+);
 
 INSERT INTO Admin(Admin_name, Contact_no, Address,Total_user, Email)
 values('Ram', 9934569889, '23-Behargao Street',0, 'Ram2334@gmail.com'),
@@ -88,10 +87,11 @@ VALUES('Ramop', 'Ram@3000', '1'),
 ('Preet', 'Pr6854', '2'),
 ('Shyamxp', 'ss7849', '3');
 /*
-SELECT* FROM Admin;
-SELECT* FROM Admin_login_system;
-SELECT* FROM Account_login_system;
-SELECT* FROM Accounts;
+some commands to check the data in the databases
+    SELECT* FROM Admin;
+    SELECT* FROM Admin_login_system;
+    SELECT* FROM Account_login_system;
+    SELECT* FROM Accounts;
+    DROP DATABASE banksystem;
 
-DROP DATABASE banksystem;
 */
